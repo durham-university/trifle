9f026c73193d1485fae5d171181cc5e530a77c5e
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2016-03-30 15:57:35 +0100

    Mirador controller can open images directly.

--- app/controllers/trifle/mirador_controller.rb
+++ app/controllers/trifle/mirador_controller.rb
@@ -2,7 +2,25 @@ module Trifle
   class MiradorController < Trifle::ApplicationController
     def show
       @resource = ActiveFedora::Base.find(params['id'])
-      raise 'Resource must be either a manifest or a collection' unless (@resource.is_a?(Trifle::IIIFManifest) || @resource.is_a?(Trifle::IIIFCollection))
+      @collection = @resource.root_collection
+      case @resource
+      when Trifle::IIIFManifest
+        @manifest = @resource
+        @image = nil
+        if params['page'].present?
+          page = params['page'].to_i-1
+          @image = @manifest.images[page] if page >= 0 && page < @manifest.images.count
+        end
+      when Trifle::IIIFImage
+        @image = @resource
+        @manifest = @image.parent
+      when Trifle::IIIFCollection
+        @manifest = nil
+        @image = nil
+      else
+          raise 'Resource must be either a collection, a manifest or an image'
+      end
+      @use_annotation = @manifest && can?(:update,@manifest)
       @no_auto_load = true if params['no_auto_load']
       render :show, layout: false
     end
