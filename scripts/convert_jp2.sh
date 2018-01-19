1eada651cb2b193293e1b54d53cf161e96767528
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2018-01-19 11:36:45 +0000

    Add watermarking into jpg2000 conversion script

--- scripts/convert_jp2.sh
+++ scripts/convert_jp2.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
+
+# This file is not directly used by the deployed application. It has its own scripts directory
+# with a similar script. The included test_app however does use this file directly.
+
 if [ $3 = "BW" ] ; then
   CS="sLUM"
 else
@@ -6,6 +10,19 @@ else
 fi  
 
 case $4 in
+  watermark)
+  SIZE=`convert "$1" -format "%wx%h" info:`
+  EXTENSION=`echo "$1" | sed 's/.*\.//'`
+  TMPFILE=`mktemp /tmp/trifle_convert.XXXXXX.$EXTENSION`
+  WATERMARKFILE="/home/qgkb58/hydra/test_watermark.png"
+  if [ $CS = "sRGB" ] ; then
+    convert -gravity center -compose dissolve -define compose:args=10% -composite -geometry $SIZE "$1" "$WATERMARKFILE" -depth 8 -type TrueColorMatte "$TMPFILE"
+  else
+    convert -gravity center -compose dissolve -define compose:args=10% -composite -geometry $SIZE "$1" "$WATERMARKFILE" -depth 8 -type GrayscaleMatte "$TMPFILE"
+  fi
+  kdu_compress -i "$TMPFILE" -o "$2" -rate 2.4,1.48331273,.91673033,.56657224,.35016049,.21641118,.13374944,.08266171 Creversible=yes Clevels=7 Cblk=\{64,64\} -jp2_space $CS Cuse_sop=yes Cuse_eph=yes Corder=RLCP ORGgen_plt=yes ORGtparts=R Stiles=\{1024,1024\} -double_buffering 10 -num_threads 4 -no_weights
+  rm $TMPFILE
+  ;;
 # "printed") kdu_compress -i "$1" -o "$2" -rate 2.4,1.48331273,.91673033,.56657224,.35016049,.21641118,.13374944,.08266171 Creversible=yes Clevels=7 Cblk=\{64,64\} -jp2_space $CS Cuse_sop=yes Cuse_eph=yes Corder=RLCP ORGgen_plt=yes ORGtparts=R Stiles=\{1024,1024\} -double_buffering 10 -num_threads 4 -no_weights ;;
   *) kdu_compress -i "$1" -o "$2" -rate 2.4,1.48331273,.91673033,.56657224,.35016049,.21641118,.13374944,.08266171 Creversible=yes Clevels=7 Cblk=\{64,64\} -jp2_space $CS Cuse_sop=yes Cuse_eph=yes Corder=RLCP ORGgen_plt=yes ORGtparts=R Stiles=\{1024,1024\} -double_buffering 10 -num_threads 4 -no_weights ;;
 esac
--- scripts/convert_ptif.sh
+++ scripts/convert_ptif.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
 
+# This file is not directly used by the deployed application. It has its own scripts directory
+# with a similar script. The included test_app however does use this file directly.
+
 convert -compress none -define tiff:tile-geometry=256x256 "$1" "ptif:$2"
